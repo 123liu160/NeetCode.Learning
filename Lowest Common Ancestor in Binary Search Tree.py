6c5c16d52ee76e1a076a9391bb9cf9feaf7cc24b
123liu160 <2501887052@qq.com>
2026-05-28 20:46:01 +0800

    Create Lowest Common Ancestor in Binary Search Tree.py

---
+++ Lowest Common Ancestor in Binary Search Tree.py
@@ -0,0 +1,20 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        cur = root
+        # 循环遍历查找
+        while cur:
+            # 如果当前值比p、q都大，两节点都在左子树
+            if cur.val > p.val and cur.val > q.val:
+                cur = cur.left
+            # 当前值比p、q都小，两节点都在右子树
+            elif cur.val < p.val and cur.val < q.val:
+                cur = cur.right
+            # 数值分居两侧/当前就是目标节点，即为答案
+            else:
+                return cur
